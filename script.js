81cea8c5a6b616a34599bf2385b3853d678572ca
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-04-12 11:12:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ const hora = document.getElementById('hora');
 const minuto = document.getElementById('minuto');
 const segundo = document.getElementById('segundo')
 
+function calcular(operador) {
+    var valor1 = parseFloat(document.getElementById('n1').value);
+    var valor2 = parseFloat(document.getElementById('n2').value);
+    var resultado;
+
+    switch (operador) {
+        case '+':
+            resultado = valor1 + valor2;
+            break;
+        case '-':
+            resultado = valor1 - valor2;
+            break;
+        case '*':
+            resultado = valor1 * valor2;
+            break;
+        case '/':
+            if (valor2 !== 0) {
+                resultado = valor1 / valor2;
+            } else {
+                alert("Não é possível dividir por zero!");
+                return;
+            }
+            break;
+        default:
+            alert("Operação inválida!");
+            return;
+    }
+
+    alert("O resultado é: " + resultado);
+}
+
 const relogio = setInterval(function time (){
      let horaAtual = new Date();
      let hr = horaAtual.getHours();
